15e22e7a9f43081967b0133407d481021bd68ce8
ddmuzyk <domino2k1@gmail.com>
2024-01-06 15:12:17 +0100

    adding move logic

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const board = document.querySelector('.board');
+const boardEl = document.querySelector('.board');
 
 const numOfRows = 15;
 
 const player = []
+const board = []
 
-const rows = []
+const direction = 'left'
 
 const createRows = (numOfRows) => {
-  for (let x = 0; x < numOfRows; x++) {
+  for (let y = 0; y < numOfRows; y++) {
     const row = [];
-    for (let y = 0; y < numOfRows; y++) {
+    for (let x = 0; x < numOfRows; x++) {
       const element = document.createElement('div');
       element.className = 'block'
-      // element.dataset.status = 'empty'
+      element.dataset.x = x
+      element.dataset.y = y
       const block = {
         element,
         x,
@@ -21,32 +23,61 @@ const createRows = (numOfRows) => {
       }
       row.push(block)
     }
-    rows.push(row)
+    board.push(row)
   }
 }
 
-const createBoard = (rows) => {
-  rows.forEach((row) => {
+const createBoard = (board) => {
+  board.forEach((row) => {
     row.forEach((block) => {
-      board.appendChild(block.element)
+      boardEl.appendChild(block.element)
     })
   })
 }
 
 const placePlayer = (numOfRows) => {
   let middle = Math.floor(numOfRows/2)
-  const block = rows[middle][middle]
+  const block = board[middle][middle]
   block.type = 'player'
   block.element.classList.add('player')
   player.push(block)
 }
 
 const move = (player, direction) => {
+  const head = player[0]
+  const x = parseInt(head.x)
+  const y = parseInt(head.y)
 
+  if (direction === 'left') {
+    const tile = board[y][x-1]
+    const gameOver = checkForLose(tile)
+    if (gameOver) return
+  } else if (direction === 'right') {
+    const tile = board[y][x+1]
+    const gameOver = checkForLose(tile)
+    if (gameOver) return
+  } else if (direction === 'up') {
+    const tile = board[y-1][x]
+    const gameOver = checkForLose(tile)
+    if (gameOver) return
+  } else if (direction === 'down') {
+    const tile = board[y+1][x]
+    const gameOver = checkForLose(tile)
+    if (gameOver) return
+  }
+}
+
+const checkForLose = (tile) => {
+  if (!tile) {
+    return true
+  } else if (tile.type === 'player') {
+    console.log('You hit yourself!')
+    return true
+  }
 }
 
 createRows(numOfRows)
-createBoard(rows)
+createBoard(board)
 placePlayer(numOfRows)
 
-console.log(rows)
+move(player, direction)
